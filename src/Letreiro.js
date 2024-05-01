e3eb94a88e4adc69f1aa5926c6d6329ba5472b8c
MariaJuliaMouraR <160803398+MariaJuliaMouraR@users.noreply.github.com>
2024-05-01 16:19:44 -0300

    Update Letreiro.js

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,9 +1,34 @@
-function Letreiro() {
-    return (
-    <>
-      <h1>Meu Letreiro</h1>
-    </>
-    );
-  }
-  
-  export default Letreiro;
+import React from 'react';
+
+const AnimatedText = () => {
+  const [text, setText] = React.useState('');
+  const originalText = 'CONHEÇA A FATEC!';
+  const delay = 200;
+
+  React.useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      if (currentIndex < originalText.length - 1) {
+        setText(prevText => prevText + originalText[currentIndex]);
+        currentIndex++;
+      } else {
+        clearInterval(interval);
+      }
+    }, delay);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>{text}</div>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <h2>LETREIRO</h2>
+      <AnimatedText />
+    </div>
+  );
+};
+
+export default App;
