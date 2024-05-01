330063afd829df95bec72f09dff41ef3849551d8
MariaJuliaMouraR <160803398+MariaJuliaMouraR@users.noreply.github.com>
2024-05-01 15:24:23 -0300

    Update Relogio.js

--- src/Relogio.js
+++ src/Relogio.js
@@ -1,9 +1,28 @@
 function Relogio() {
-  return (
-  <>
     <h1>Meu Relógio</h1>
-  </>
+  const [hora, setHora] = useState('');
+
+  useEffect(() => {
+    const atualizarHora = () => {
+      let relogio = new Date();
+      relogio.setTime(relogio.getTime() - 3 * 60 * 60 * 1000);
+      let horaAtual = relogio.toISOString().substring(11, 19);
+      setHora(horaAtual);
+    };
+
+    const intervalID = setInterval(atualizarHora, 1000);
+
+    return () => clearInterval(intervalID);
+  }, []);
+
+  return (
+    <div>
+      <h4>Hora atual:</h4>
+      <p>{hora}</p>
+    </div>
+    
   );
 }
+  </>
 
 export default Relogio;
