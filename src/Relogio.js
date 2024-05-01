7ba91e5acf652dc1bc0200989746322c7dfc6f24
MariaJuliaMouraR <160803398+MariaJuliaMouraR@users.noreply.github.com>
2024-05-01 16:24:09 -0300

    Update Relogio.js

--- src/Relogio.js
+++ src/Relogio.js
@@ -1,9 +1,40 @@
-function Letreiro() {
-    return (
+import React, { useState, useEffect } from 'react';
+
+function Relogio() {
+  const [hora, setHora] = useState('');
+  const [dia, setDia] = useState('');
+  const [mes, setMes] = useState('');
+  const [ano, setAno] = useState('');
+
+  useEffect(() => {
+    const atualizarHoraData = () => {
+      let relogio = new Date();
+      relogio.setTime(relogio.getTime() - 3 * 60 * 60 * 1000); // Ajuste de fuso horário, se necessário
+      let horaAtual = relogio.toISOString().substring(11, 19);
+      let diaAtual = relogio.getDate();
+      let mesAtual = relogio.getMonth() + 1; // Mês começa do zero, por isso adicionamos 1
+      let anoAtual = relogio.getFullYear();
+
+      setHora(horaAtual);
+      setDia(diaAtual);
+      setMes(mesAtual);
+      setAno(anoAtual);
+    };
+
+    const intervalID = setInterval(atualizarHoraData, 1000);
+
+    return () => clearInterval(intervalID);
+  }, []);
+
+  return (
     <>
-      <h1>Meu Letreiro</h1>
+    <div>
+      <h2>DATA E HORA</h2>
+      <p>{`${dia}/${mes}/${ano}`}</p>  
+      <p>{hora}</p>
+    </div>
     </>
-    );
-  }
-  
-  export default Letreiro;
+  );
+}
+
+export default Relogio;
